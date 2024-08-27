aaaf0f8cd3b82ebbd333752bf9883e3698aaf947
sagarsinha1803 <sagarsinha1803@gmail.com>
2024-08-27 23:12:20 +0530

    initail commit 25

--- manage.py
+++ manage.py
@@ -2,14 +2,16 @@ from flask import Flask
 from .service.service import hello_world_fn
 
 app = Flask(__name__)
-app.config['TESTING'] = True
+
 
 @app.route("/")
 def hello_world():
     return hello_world_fn()
 
+
 def run():
-     app.run(host="0.0.0.0", port=8004, debug=True)
+    app.run(host="0.0.0.0", port=8004, debug=True)
+
 
 if __name__ == "__main__":
     run()
