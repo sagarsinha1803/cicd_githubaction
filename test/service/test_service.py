03c268a8d88a0ce71e0ca0e6f40b3ef5559bd3bf
sagarsinha1803 <sagarsinha1803@gmail.com>
2024-08-27 22:14:43 +0530

    initail commit 16

--- test/service/test_service.py
+++ test/service/test_service.py
@@ -1,11 +1,14 @@
-import unittest
+import pytest
 from service.service import hello_world_fn
+import os
+import sys
 
-class TestService(unittest.TestCase):
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
-    def test_hello_world_fn(self):
-        res = hello_world_fn()
-        self.assertEqual(res, b'Hello, World!')
+
+def test_hello_world_fn():
+    res = hello_world_fn()
+    assert res == 'Hello World!'
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
